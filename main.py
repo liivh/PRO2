eedfd2f14830fa8f00b93c018832e5d58ff94d68
ihr_benutzernameliiv_h <71707861+liivh@users.noreply.github.com>
2020-11-27 09:05:35 +0100

    add kursangebot

--- main.py
+++ main.py
@@ -2,19 +2,17 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+
 from libs.daten import speichern
 
 app = Flask("Kursfinder")
 
 
-# git test
-
 @app.route('/')
 def start():
     ueberschrifts_text = "Willkommen auf der Kursfinder Website"
     einleitung_text = "Hier kannst du herausfinden, welchen Pfadikurs du als nächstes besuchen kannst"
-    return render_template("start.html", app_name="Kursfinder", ueberschrift=ueberschrifts_text,
-                           einleitung=einleitung_text)
+    return render_template("start.html", app_name="Kursfinder", ueberschrift=ueberschrifts_text, einleitung=einleitung_text)
 
 
 @app.route('/eingabe', methods=['POST', 'GET'])
@@ -27,6 +25,25 @@ def eingabe():
         antwort = speichern(name, jahrgang, kurs, jahr)
         return 'Gespeicherete Daten: <br>' + str(antwort)
     return render_template('eingabe.html', app_name="Kursfinder - Eingabe")
+
+
+"""
+@app.route('/kursangebot', methods=['POST', 'GET'])
+def kursangebot():
+    if request.method == 'POST':
+        kursangebot = request.form
+        print (kursangebot)
+        komplettes_kursangebot = read_data_from_json("datenbank.json")
+        jahrgang = kursangebot['Jahrgang']
+        kurs = kursangebot['Kurs']
+        jahr = kursangebot['Jahr']
+        liste = []
+
+        for eintrag in komplettes_kursangebot:
+            if eintrag["jahrgang"] == range(0,2004) and eintrag["kurs"] == 'Basiskurs':
+
+"""
+
 
 
 
