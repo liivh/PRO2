5d8bf7426a3b83b6f2900d8c64522186c66d9182
ihr_benutzernameliiv_h <71707861+liivh@users.noreply.github.com>
2020-12-03 13:34:34 +0100

    antworten kursangebot

--- libs/daten.py
+++ libs/daten.py
@@ -25,26 +25,41 @@ def laden ():
         eintraege = []
 
     return eintraege
-
 """
-import json
-
-
-# testing
-def speichern(nummer, jahrgang, kurs, jahr):
+def kursangebot_oeffnen():
     try:
-        with open("datenbank.json", "r") as datenbank:
-            eintraege = json.load(datenbank)
+        with open("datenbank.json", "r", encoding="utf-8") as datenbank_kursangebot:
+            kursangebot = json.load(datenbank_kursangebot)
     except:
-        eintraege = {}
+        kursangebot = {}
+    return kursangebot
 
-    eintraege[nummer] = {"Jahrgang": jahrgang, "Kurs": kurs, "Jahr": jahr}
+def kursangebot_speichern(kursname_kursangebot_antwort,
+                          jahrgang_antwort,
+                          gueltigkeit_antwort,
+                          voraussetzung_antwort,
+                          fortsetzung_antwort):
+    kursangebot = kursangebot_oeffnen()
 
+    kursangebot = {
+        name_kursangebot_antwort:{
+            "kursname": kursname_kursangebot_antwort,
+            "jahrgang": jahrgang_antwort,
+            "voraussetzung": voraussetzung_antwort,
+            "fortsetzung": fortsetzung_antwort,
+        }
+    }
+kursangebot_update(kursangebot)
 
-    with open("datenbank.json", "w") as datenbank:
-        json.dump(eintraege, datenbank, indent=4)
-    return "Daten gespeichert"
+with open("datenbank.json", "w") as datenbank_kursangebot:
+    json.dump(kursangebot, datenbank)
 
+return
+    name_kursangebot_antwort, \
+    jahrgang_antwort, \
+    gueltigkeit_antwort, \
+    voraussetzung_antwort, \
+    fortsetzung_antwort
 
-# end of testing
 """
+
